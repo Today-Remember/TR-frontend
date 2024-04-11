53b3f815af263f59fa96b4b9b892fa85b24164ad
tlswl7479 <tlswl7479@gmail.com>
2024-04-11 10:39:03 +0900

    .

--- client/src/components/IntroPage/IntroPage.jsx
+++ client/src/components/IntroPage/IntroPage.jsx
@@ -4,13 +4,15 @@ const IntroPage = () => {
   return (
     <div>
       <form className="input">
-        <input type="text" />
+        <h3>아이디</h3>
+        <input type="text" id="id" />
         <br></br>
+        <h3>비밀번호</h3>
         <input type="password" id="pw" />
         <br></br>
-        <button type="submit">로그인</button>
+        <button className="login" type="submit">로그인</button>
       </form>
-      <button>회원가입</button>
+      <button className="join_member">회원가입</button>
     </div>
   );
 };
