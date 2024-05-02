c5ee16eedec6d5b915175042827ee7c4934ef969
tlswl7479 <tlswl7479@gmail.com>
2024-05-02 20:23:29 +0900

    인트로페이지 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import IntroPage from "./components/IntroPage/IntroPage";
-import MainPage from "./components/main/MainPage";
+import MainPage from "./components/main/mainPage";
+import DetailPage from "./components/detailPage/detailPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<IntroPage/>} />
-        <Route path="/main" element={<MainPage/>} />
+        <Route path="/main" element={<MainPage/>}/>
+        <Route path="/detail" element={<DetailPage/>}/>
       </Routes>
     </Router>
   );
