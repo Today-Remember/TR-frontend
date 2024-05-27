3b7da28dba6e0360d0f0a160ea57998411eb0599
vvalvvizal <vvalvvizal@gmail.com>
2024-05-28 00:23:37 +0900

    feat : 반응형 디자인 추가, signin 페이지 디자인 수정, main페이지 디자인 수정

--- client/src/components/signin/SigninPage.jsx
+++ client/src/components/signin/SigninPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./SigninPage.css";
-
+import { ReactComponent as Kakao } from "../signin/SocialLogin/icon/kakao.svg";
+import { ReactComponent as Naver } from "../signin/SocialLogin/icon/naver.svg";
 const handleSubmit = () => {
   // console.log('전송할 정보:', inputValue);
   // 전송 후 입력값 초기화
@@ -8,44 +9,59 @@ const handleSubmit = () => {
 };
 
 const SigninPage = () => {
-  return( 
-  <div>
-    <div className="social_login_container">
-      <div className="socail_login_text">
-        다른 소셜 계정으로 연결
-      </div>
-      <div className="social_login_box">
-        <div className="naver_box">
-          <h3>네이버</h3>
+  return (
+    <div className="content">
+      <div className="social_login_container">
+        <h3>다른 소셜 계정으로 연결</h3>
+        <div className="social_login_box">
+          <div>
+            <Kakao />
+          </div>
+          <div>
+            <Naver />
+          </div>
         </div>
-        <div className="kakao_box">
-          <h3>카카오</h3>
-        </div>  
-      </div>    
-    </div>
-    <hr />
-    <div className="signin_info_box">
-      <div className="signin_container">
-        별명
-        <input className="signin_box"></input>
       </div>
-      <div className="signin_container">
-        아이디
-        <input className="signin_box"></input>
-      </div>
-      <div className="signin_container">
-        비밀번호
-        <input className="signin_box"></input>
-      </div>
-      <div className="signin_container">
-        이메일
-        <input className="signin_box"></input>
+      <div className="signin_info">
+        <div className="signin_container">
+          <p>별명</p>
+          <input
+            className="signin_box"
+            type="text"
+            placeholder="별명을 입력해주세요."
+          ></input>
+        </div>
+        <div className="signin_container">
+          <p>아이디</p>
+          <input
+            className="signin_box"
+            type="text"
+            placeholder="아이디를 입력해주세요."
+          ></input>
+        </div>
+        <div className="signin_container">
+          <p>비밀번호</p>
+          <input
+            className="signin_box"
+            type="password"
+            placeholder="비밀번호를 입력해주세요."
+          ></input>
+        </div>
+        <div className="signin_container">
+          <p>비밀번호 확인</p>
+          <input
+            className="signin_box"
+            type="password"
+            placeholder="이메일을 입력해주세요"
+          ></input>
+        </div>
       </div>
       <div className="submit_button_container">
-        <button className="submit_button" onClick={handleSubmit}>Sign up</button>
+        <button className="submit_button" onClick={handleSubmit}>
+          <p>Sign up</p>
+        </button>
       </div>
     </div>
-  </div>
-  )
+  );
 };
 export default SigninPage;
