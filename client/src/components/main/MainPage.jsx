3b5a2af30b50269462007d5058a40cdd120ba432
tlswl7479 <tlswl7479@gmail.com>
2024-06-01 16:53:30 +0900

    .

--- client/src/components/main/MainPage.jsx
+++ client/src/components/main/MainPage.jsx
@@ -1,8 +1,25 @@
-import React from "react";
-import SendButton from "./Send/SendButton";
+import React, { useState } from "react";
 import "./MainPage.css";
+import axios from "axios";
 
 const MainPage = () => {
+  const [inputText, setInputText] = useState("");
+
+  const handleInput = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleClick = async () => {
+    try {
+      const response = await axios.post("http://localhost:8000/text", {
+        text: inputText,
+      });
+      console.log("response: ", response.data);
+    } catch (error) {
+      console.log("Error!, data: ", error);
+    }
+  };
+
   const text =
     "오늘은 오전은 맑은 날씨였지만 바람이 많이 불었다. 오후에는 비가 조금 내렸다.비가와서 우산을 쓰고 집으로 왔다.";
   return (
@@ -11,10 +28,10 @@ const MainPage = () => {
         <div className="text">{text}</div>
       </div>
       <div className="input_box">
-        <input className="input"></input>
-        <div className="send-button">
-          <SendButton />
-        </div>
+        <input className="input" value={inputText} onChange={handleInput}></input>
+        <button className="button" onClick={handleClick}>
+          분석하기
+        </button>
       </div>
     </div>
   );
