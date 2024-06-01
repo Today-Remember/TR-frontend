765c81fb85836c22cadf00000b7283e24b5fc1fd
tlswl7479 <tlswl7479@gmail.com>
2024-06-01 22:45:37 +0900

    .

--- client/src/components/main/MainPage.jsx
+++ client/src/components/main/MainPage.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./MainPage.css";
 import axios from "axios";
 
 const MainPage = () => {
   const [inputText, setInputText] = useState("");
+  const [receivedText, setReceivedText] = useState("");
 
   const handleInput = (e) => {
     setInputText(e.target.value);
@@ -20,17 +21,31 @@ const MainPage = () => {
     }
   };
 
-  const text =
-    "오늘은 오전은 맑은 날씨였지만 바람이 많이 불었다. 오후에는 비가 조금 내렸다.비가와서 우산을 쓰고 집으로 왔다.";
+  // 백엔드 데이터 수신
+  useEffect(() => {  
+    const fetchText = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/text");
+        setReceivedText(response.data.received_text);
+      } catch (error) {
+        console.log("Error!, data: ", error);
+      }
+    };
+
+    fetchText();
+  }, []);
+
+  // const text =
+  //   "오늘은 오전은 맑은 날씨였지만 바람이 많이 불었다. 오후에는 비가 조금 내렸다.비가와서 우산을 쓰고 집으로 왔다.";
   return (
-    <div>
+    <div className="container">
       <div className="show_text_box">
-        <div className="text">{text}</div>
+        <div className="text">{receivedText}</div>
       </div>
-      <div className="input_box">
-        <input className="input" value={inputText} onChange={handleInput}></input>
-        <button className="button" onClick={handleClick}>
-          분석하기
+      <div className="main_input_box">
+        <input className="input" value={inputText} onChange={handleInput} />
+        <button className="send-button" onClick={handleClick}>
+          보내기
         </button>
       </div>
     </div>
